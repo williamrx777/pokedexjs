120e7654f857a70ce13002dc7da82b3d437ddb9a
william rodrigues <williamr723@gmail.com>
2023-11-01 16:57:22 -0300

    adicionando habilidade e movimento

--- aula.js
+++ aula.js
@@ -12,13 +12,17 @@ function searchPokemon(){
     .then(data => {
         document.getElementById('pokemonName').textContent = `Name : ${data.name}`;
         document.getElementById('pokemonImage').src = data.sprites.front_default;
+        document.getElementById('pokemonAbility').textContent = `Ability : ${data.abilities[0].ability.name}`
+        document.getElementById('pokemonMove').textContent = `Move : ${data.moves[0].move.name}`
         
     })
     .catch(error => {
         document.getElementById('pokemonName').textContent = 'Pokemon not found.';
         document.getElementById('pokemonImage').src = '';
-        
+        document.getElementById('pokemonAbility').textContent = 'Ability not found'
+        document.getElementById('pokemonMove').textContent = 'Move not found'
         console.error('Error', error);
-    });
+    })
+    
 }
     
